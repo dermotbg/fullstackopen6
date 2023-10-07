3de87d3efd359b7c27b266be70ecefabdae785db
Dermot Bateman <dermotbateman@gmail.com>
2023-10-07 13:35:15 +0300

    done: 6.11 / anecdote reducer / action creators

--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -2,13 +2,13 @@ import { useSelector, useDispatch } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  
+
   const anecdotes = useSelector(state => {
     if (state.filter === ''){
-      return state.anecdotes
+      return [...state.anecdotes].sort((a, b) => b.votes - a.votes)
     }
-    return state.anecdotes.filter((a) => a.content.toLowerCase().includes(state.filter.toLowerCase()))
-  }).sort((a, b) => b.votes - a.votes)
+    return [...state.anecdotes.filter((a) => a.content.toLowerCase().includes(state.filter.toLowerCase()))].sort((a, b) => b.votes - a.votes)
+}) 
 
   const dispatch = useDispatch()
 
--- redux-anecdotes/src/reducers/filterReducer.js
+++ redux-anecdotes/src/reducers/filterReducer.js
@@ -1,18 +1,13 @@
-const filterReducer = (state = '', action) => {
-  console.log('action', action)
-  switch (action.type) {
-    case 'SET_FILTER':
-      return action.payload
-    default: 
-      return state
-  }
-}
+import { createSlice } from "@reduxjs/toolkit"
 
-export const filterChange = (filter) => {
-  return{
-    type: 'SET_FILTER',
-    payload: filter
-  }
-}
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    filterChange: (state, action) => action.payload,
+    },
+})
 
-export default filterReducer
+export const { filterChange } = filterSlice.actions
+
+export default filterSlice.reducer
