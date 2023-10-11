75eca5871749471ee37c159761e50343d3d49a52
Dermot Bateman <dermotbateman@gmail.com>
2023-10-11 11:02:41 +0300

    done: 6.20 / get req with react-query

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,5 +1,7 @@
+import { useQuery } from '@tanstack/react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAll } from './requests'
 
 const App = () => {
 
@@ -7,13 +9,17 @@ const App = () => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAll,
+    refetchOnWindowFocus: false
+  })
+  
+  if ( result.isLoading ) return <div>loading data...</div>
+  if ( result.isError ) return <div>error connecting to server</div> 
+
+  const anecdotes = result.data
 
   return (
     <div>
